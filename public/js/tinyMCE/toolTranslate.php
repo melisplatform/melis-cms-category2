0af0bf29116de13c7fe9e534a24b8c29f6999f75
Junry Zabate <jzabajte@melistechnology.com>
2019-06-24 12:49:23 +0800

    fixed issue: http://mantis.melistechnology.fr/view.php?id=3991 update tinymce init config for tinymce v5

--- public/js/tinyMCE/toolTranslate.php
+++ public/js/tinyMCE/toolTranslate.php
@@ -13,9 +13,10 @@ return array(
     'setup' => 'melisTinyMCE.tinyMceActionEvent',
     'paste_auto_cleanup_on_paste' => true,
     'plugins' => array(
-        'advlist autolink lists link image charmap print preview anchor',
+        //[contextmenu, textcolor, colorpicker] this plugin is already built in the core editor as of TinyMCE v. 5
+        'lists advlist autolink link image charmap print preview anchor',
         'searchreplace visualblocks code fullscreen',
-        'insertdatetime media table contextmenu paste'
+        'insertdatetime media table paste'
     ),
     'toolbar' => 'undo redo | styleselect | bold italic | link image |  alignleft aligncenter alignright alignjustify',
     'init_instance_callback' => 'tinyMceCleaner',
